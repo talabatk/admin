b29b481ae297542ab7109a5601fdf9182c8940d4
omar mohamed <omer.eldanin@gmail.com>
2024-12-15 20:34:10 +0200

    first commit

--- src/components/layout/Navbar/Notifications/NotificationItem/NotificationItem.js
+++ src/components/layout/Navbar/Notifications/NotificationItem/NotificationItem.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import "./notificationItem.scss";
 const NotificationItem = (props) => {
   return (
-    <NavLink to={`order/${props.orderId}`} className="NotificationItem">
+    <NavLink to={`orders/${props.orderId}`} className="NotificationItem">
       <div className="content">
         <p>{props.title}</p>
         <span>{props.description}</span>
